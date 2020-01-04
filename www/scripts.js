da9629c65ef6d4bac1f156647aaab35491eb238a
enbot <nimbotelho@gmail.com>
2020-01-04 18:47:20 -0300

    new custom 64

--- www/scripts.js
+++ www/scripts.js
@@ -23,17 +23,39 @@ function tests() {
     console.log(key)
     console.log(value)
 
-    let encoded = av64x4.encode(value)
-    console.log('encoded:')
-    console.log(encoded)
+    // let encoded = av64x4.encode(value)
+    // console.log('encoded:')
+    // console.log(encoded)
 
-    let decoded = av64x4.decode(encoded)
-    console.log('decoded:')
-    console.log(decoded)
+    // let decoded = av64x4.decode(encoded)
+    // console.log('decoded:')
+    // console.log(decoded)
 
-    let when = av64x4.when(encoded)
-    console.log('when:')
-    console.log(when)
+    // let when = av64x4.when(encoded)
+    // console.log('when:')
+    // console.log(when)
+
+    console.log('/==========================================================================/');
+
+    let myjson = {
+        test: true,
+        value: 2,
+        myarray: [1, 2, 3, 4, 5, 6],
+        a: '( ͡° ͜ʖ ͡°)'
+    }
+    console.log(myjson);
+
+    let stringfied = JSON.stringify(myjson)
+    console.log(stringfied);
+
+    let jsonencoded = av64x4.encode(stringfied)
+    console.log(jsonencoded);
+
+    let jsondecoded = av64x4.decode(jsonencoded)
+    console.log(jsondecoded);
+
+    let jsonparsed = JSON.parse(jsondecoded)
+    console.log(jsonparsed);
 
     console.log('/==========================================================================/');
 
